6d782573d9b3698b078c7e014e8fba5f12113eec
Mitali Acharya <mitalinileshacharya@gmail.com>
2025-04-25 00:29:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def ld(path="IPL_2018_2024.xlsx"):
     cols = [
         "bat", "batruns", "out", "dismissal",
         "wagonX", "wagonY", "wagonZone", "line", "length",
-        "bowl_style", "year", "bat_hand"
+        "bowl_style", "year", "bat_hand", "over", "ballfaced"
     ]
     df = df[[c for c in cols if c in df.columns]].drop_duplicates()
     if "year" in df.columns:
